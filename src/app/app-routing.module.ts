58f8c7e46c888cfa9effc87787524a1040520d51
ÐSergey Dziadevich <developersiteweb@gmail.com>
2020-03-21 16:03:02 +0300

    add routing, admin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+  },
+  {
+    path: 'admin',
+      // loadChildren: './admin/admin.module#AdminModule'
+      loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule),
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules // loading the admin module in the background
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
