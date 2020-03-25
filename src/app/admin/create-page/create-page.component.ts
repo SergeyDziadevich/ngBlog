0c19ef71323ed6dbf39e50c62d65c30391ddb369
ÐSergey Dziadevich <developersiteweb@gmail.com>
2020-03-25 17:18:12 +0300

    PostsService

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {Post} from '../../shared/interfaces';
+import {PostsService} from '../../shared/services/posts.service';
 
 @Component({
   selector: 'app-create-page',
@@ -11,7 +12,7 @@ export class CreatePageComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -33,6 +34,11 @@ export class CreatePageComponent implements OnInit {
       text: this.form.value.text,
       date: new Date()
     };
+
+    this.postsService.create(post).subscribe((id) => {
+      console.log(id);
+      this.form.reset();
+    });
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Enviroment} from './interface';
 
 export const environment: Enviroment = {
   production: false,
-  apiKey: 'AIzaSyDukWLMk2462WtaXL74SL-2HBZH9ga2rGU'
+  apiKey: 'AIzaSyDukWLMk2462WtaXL74SL-2HBZH9ga2rGU',
+  fbDBUrl: 'https://ngblog-41795.firebaseio.com'
 };
