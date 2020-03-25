2b8c424709e882576363fe80ae3fc8cc064f4e8a
ÐSergey Dziadevich <developersiteweb@gmail.com>
2020-03-25 16:35:37 +0300

    add plugin quill

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    QuillModule.forRoot(),
     HttpClientModule
   ],
-  exports: [HttpClientModule]
+  exports: [
+    HttpClientModule,
+    QuillModule
+  ]
 })
 export class SharedModule { }
