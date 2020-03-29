3febe732498ed4a73b9c41becd41b24034b89bc5
ÐSergey Dziadevich <developersiteweb@gmail.com>
2020-03-29 21:10:48 +0300

    edit page

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+import {PostsService} from '../../shared/services/posts.service';
+import {switchMap} from 'rxjs/operators';
+import {Post} from '../../shared/interfaces';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  post: Post;
+  form: FormGroup;
+  submitted = false;
+
+  private uSub: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postsService.getPostById(params.id);
+        })
+      ).subscribe((post: Post) => {
+        this.post = post;
+        this.form = new FormGroup({
+            title: new FormControl(post.title, Validators.required),
+            text: new FormControl(post.text, Validators.required)
+          }
+        );
+    });
   }
 
+  ngOnDestroy(): void {
+   if (this.uSub) {
+     this.uSub.unsubscribe();
+   }
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.submitted = true;
+
+    this.uSub = this.postsService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(() => {
+      this.submitted = false;
+    });
+  }
 }
